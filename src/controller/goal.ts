3adc8771e48488a2cdde31d0a325cc90df160d04
Michael Curry <michaelcurry95@gmail.com>
2021-10-03 15:19:26 -0400

    some more controller shells

--- src/controller/goal.ts
+++ src/controller/goal.ts
@@ -2,6 +2,8 @@ export async function createGoal(goal: string) {}
 
 export async function getGoals() {}
 
+export async function getGoal(goalId: number) {}
+
 export async function updateGoal(goalId: number, goal: string) {}
 
 export async function deleteGoal(goalId: number) {}
--- src/controller/project.ts
+++ src/controller/project.ts
@@ -1,5 +1,7 @@
 export async function createProject(idea: string, description: string, goalId?: number) {}
 
+export async function getProject(projectId: number) {}
+
 export async function getProjects() {}
 
 export async function addGoal(projectId: number, goalId: number) {}
