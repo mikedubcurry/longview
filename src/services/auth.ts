0b42cba324dba6a0d9941913ff6f4e6d835db111
Michael Curry <michaelcurry95@gmail.com>
2021-10-04 18:59:38 -0400

    start tests for auth service

---
+++ src/services/auth.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+
+export async function logIn(req: Request, res: Response) {}
+
+export async function signUp(req: Request, res: Response) {}
+
+export async function logOut(req: Request, res: Response) {}
+
+export async function deleteUser(req: Request, res: Response) {}
---
+++ src/services/tests/auth.spec.ts
@@ -0,0 +1,18 @@
+import supertest from 'supertest';
+
+import { app } from '../../app';
+import { routes } from '../../routes';
+
+describe('auth service', () => {
+	it('should verify auth test suite runs', () => {
+		expect(1 + 1).toEqual(2);
+	});
+
+	// test for logIn handler
+
+	// test for logOut handler
+
+	// test for signUp handler
+
+	// test for deleteUser handler
+});
