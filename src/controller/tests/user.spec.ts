b412981115e43a47556695f42d91bf8dd0be19bd
Michael Curry <michaelcurry95@gmail.com>
2021-10-04 17:34:35 -0400

    more user controller fns and tests

--- src/controller/tests/user.spec.ts
+++ src/controller/tests/user.spec.ts
@@ -1,8 +1,9 @@
 import { config } from 'dotenv';
 config();
-import { createUser, deleteUser } from '../user';
+import { commparePassword, createUser, deleteUser, userExists } from '../user';
 import { sqlConnection } from '../../db';
 import { dbInit } from '../../db/init';
+import { assert } from 'console';
 
 describe('user controller', () => {
 	it('should create a user in the test db', async () => {
@@ -27,15 +28,55 @@ describe('user controller', () => {
 	it('should delete a user', async () => {
 		const userPass = 'deleteMe';
 		await sqlConnection.authenticate({ logging: false });
-		// await dbInit();
 		await sqlConnection.sync({ force: true, alter: true });
 
 		await createUser(userPass, userPass);
 
 		await deleteUser(userPass, userPass);
-		const [ result] = await sqlConnection.query(`select "deletedAt" from users where username = 'deleteMe';`);
+		const [result] = await sqlConnection.query(`select "deletedAt" from users where username = 'deleteMe';`);
 
 		expect(result).toHaveLength(1);
+	});
+
+	it('should throw if to be deleted user does not exist', async () => {
+		await sqlConnection.authenticate({ logging: false });
+		await sqlConnection.sync({ force: true, alter: true });
+		const username = 'mike';
+		const password = 'doesntmatter';
+
+		await expect(deleteUser(username, password)).rejects.toThrow();
+	});
+
+	it('should check if a user exists', async () => {
+		await sqlConnection.authenticate({ logging: false });
+		await sqlConnection.sync({ force: true, alter: true });
+		const username = 'jotoro';
+		const password = 'password';
+		await createUser(username, password);
+
+		let exists = await userExists(username);
+
+		expect(exists).toBeTruthy();
+
+		let doesNotExist = await userExists('dio');
+
+		expect(doesNotExist).toBeFalsy();
+	});
+
+	it('should return true if username and password match', async () => {
+		await sqlConnection.authenticate({ logging: false });
+		await sqlConnection.sync({ force: true, alter: true });
+		const username = 'jotoro';
+		const password = 'jojo';
+		await createUser(username, password);
+
+		let passwordsMatch = await commparePassword(username, password);
+
+		expect(passwordsMatch).toEqual(true);
+
+		let passwordsDontMatch = await commparePassword('username', 'dio');
+
+		expect(passwordsDontMatch).toEqual(false);
 	});
 });
 
--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,6 +1,11 @@
+import { compareSync } from 'bcrypt';
 import { User } from '../model';
 
-export async function logIn(username: string, password: string) {}
+export async function commparePassword(username: string, password: string) {
+	const user = await userExists(username);
+	if (!user) return false;
+	return compareSync(password, user.password);
+}
 
 export async function createUser(username: string, password: string) {
 	const user = new User({ username, password });
@@ -9,5 +14,12 @@ export async function createUser(username: string, password: string) {
 }
 
 export async function deleteUser(username: string, password: string) {
+	const exists = await userExists(username);
+	if (!exists) throw new Error(`user: ${username} does not exist`);
 	return await User.destroy({ where: { username: username } });
+}
+
+export async function userExists(username: string) {
+	const user = await User.findOne({ where: { username } });
+	return user;
 }
