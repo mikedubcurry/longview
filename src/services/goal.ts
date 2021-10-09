b12e747fb6544ea028028f2d4ffbc84114f3f97c
Michael Curry <michaelcurry95@gmail.com>
2021-10-09 16:22:05 -0400

    outline goal service methods

---
+++ src/services/goal.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+
+export async function getAllGoals(req: Request, res: Response) {}
+
+export async function getGoalById(req: Request, res: Response) {}
+
+export async function createNewGoal(req: Request, res: Response) {}
+
+export async function updateGoalById(req: Request, res: Response) {}
+
+export async function deleteGoalById(req: Request, res: Response) {}
