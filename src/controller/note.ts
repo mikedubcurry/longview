1f02c12cd43ff50d306703a760ac2e780d4fa02d
Michael Curry <michaelcurry95@gmail.com>
2021-10-23 18:47:04 -0400

    include ownerId in note controller

--- src/controller/note.ts
+++ src/controller/note.ts
@@ -1,9 +1,9 @@
-export async function createNote(text: string) {}
+export async function createNote(text: string, ownerId: number) {}
 
-export async function getProjectNotes(projectId: number) {}
+export async function getProjectNotes(projectId: number, ownerId: number) {}
 
-export async function getNotes() {}
+export async function getNotes(ownerId: number) {}
 
-export async function updateNote(noteId: number, text: string) {}
+export async function updateNote(noteId: number, text: string, ownerId: number) {}
 
-export async function deleteNote(noteId: number) {}
+export async function deleteNote(noteId: number, ownerId: number) {}
