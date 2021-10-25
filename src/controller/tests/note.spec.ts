8cf6bcbd680ac62538a8b05cc734082e1accb3dd
Michael Curry <michaelcurry95@gmail.com>
2021-10-24 20:44:02 -0400

    outline notes controller tests

---
+++ src/controller/tests/note.spec.ts
@@ -0,0 +1,17 @@
+import { config } from 'dotenv';
+config();
+import { createUser } from '../user';
+import { createProject } from '../project';
+import { sqlConnection } from '../../db';
+
+const db = sqlConnection;
+
+let user: { id: number };
+let project: { id: number };
+
+describe('note controller', () => {
+	beforeAll(async () => {
+		user = await createUser('notecontroller', 'notecontroller');
+		project = await createProject('noteController', 'noteController', user.id);
+	});
+});
