7c2090dfae3259420c5316ead3b5a88ae7f4c1a5
Michael Curry <michaelcurry95@gmail.com>
2021-11-06 20:18:25 -0400

    protected routes with react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,107 @@
-import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Switch, Route, useHistory } from 'react-router-dom';
+import { useEffect, useState, useContext, createContext } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect, useHistory } from 'react-router-dom';
 
 import './App.css';
 import Nav from './components/Nav';
 
-function App() {
-	const [loggedIn, setLoggedIn] = useState(false);
+const fakeAuth = {
+	isAuthenticated: false,
+	signin(cb) {
+		fakeAuth.isAuthenticated = true;
+		setTimeout(cb, 100); // fake async
+	},
+	signout(cb) {
+		fakeAuth.isAuthenticated = false;
+		setTimeout(cb, 100);
+	},
+};
 
-	// useEffect(() => {
-	// 	if (!loggedIn) {
-  //     window.location = '/'
-	// 	}
-	// }, [loggedIn]);
+const authContext = createContext();
+
+function useAuth() {
+	return useContext(authContext);
+}
+
+function PrivateRoute({ children, ...rest }) {
+	let auth = useAuth();
+	return (
+		<Route
+			{...rest}
+			render={({ location }) =>
+				auth.user ? (
+					children
+				) : (
+					<Redirect
+						to={{
+							pathname: '/',
+							state: { from: location },
+						}}
+					/>
+				)
+			}
+		/>
+	);
+}
+
+export function ProvideAuth({ children }) {
+	const auth = useProvideAuth();
+	return <authContext.Provider value={auth}>{children}</authContext.Provider>;
+}
+
+function useProvideAuth() {
+	const [user, setUser] = useState(null);
+
+	const signin = (cb) => {
+		return fakeAuth.signin(() => {
+			setUser('user');
+			cb();
+		});
+	};
+
+	const signout = (cb) => {
+		return fakeAuth.signout(() => {
+			setUser(null);
+			cb();
+		});
+	};
+
+	return {
+		user,
+		signin,
+		signout,
+	};
+}
+
+function App() {
+	let history = useHistory();
+	let auth = useAuth();
+	let { from } = location.state || { from: { pathname: '/' } };
+	let login = () => {
+		auth.signin(() => {
+			history.replace(from);
+		});
+	};
 
 	return (
 		<div className="App">
-			<Router>
-				<Nav loggedIn={loggedIn} />
-				<button onClick={() => setLoggedIn(!loggedIn)}>{loggedIn ? 'Log out' : 'Log in'}</button>
-				<Switch>
-					<Route path="/about">About</Route>
-					<Route path="/goals">Goals</Route>
-					<Route path="/projects">Projects</Route>
-					<Route path="/">Home</Route>
-				</Switch>
-			</Router>
+			<Nav loggedIn={auth && auth.user} />
+			{auth && auth.user ? (
+				<button
+					onClick={() => {
+						auth.signout(() => history.push('/'));
+					}}
+				>
+					Log out
+				</button>
+			) : (
+				<button onClick={login}>Log in</button>
+			)}
+			<Switch>
+				<Route path="/about">About</Route>
+				<PrivateRoute path="/goals">Goals</PrivateRoute>
+				<PrivateRoute path="/projects">Projects</PrivateRoute>
+				<Route path="/">Home</Route>
+			</Switch>
 		</div>
 	);
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App, { ProvideAuth } from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+	<React.StrictMode>
+		<ProvideAuth>
+			<Router>
+				<App />
+			</Router>
+		</ProvideAuth>
+	</React.StrictMode>,
+	document.getElementById('root')
+);
