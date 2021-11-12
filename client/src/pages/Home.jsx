d1ade9dc4829e736c07e9c2ffdf020aa12442038
Michael Curry <michaelcurry95@gmail.com>
2021-11-12 16:55:20 -0500

    working on some animated svg logos

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import { FeatureSection } from '../components/FeatureSection';
 import { Hero } from '../components/Hero';
+import { Logo } from '../components/Logo';
 
 export function Home() {
 	return (
 		<>
 			<Hero title="Longview" subtitle="Keeping track of all the things so you don't have to."></Hero>
 			<main>
+				<Logo />
 				<FeatureSection
+					// add featureIcon svg
 					featureText={{
 						__html:
 							'Keep track of different <em>Goals</em> and <em>Projects</em> while documenting your successes andchallanges.',
@@ -14,6 +17,7 @@ export function Home() {
 				/>
 
 				<FeatureSection
+					// add featureIcon svg
 					featureText={{
 						__html:
 							'Coming soon, allow other users to join in a group and collaborate with shared <em>Goals</em> and <em>Projects</em>',
