0d7026a37c6a14437487eb750f5631881610f997
Michael Curry <michaelcurry95@gmail.com>
2021-11-12 17:30:12 -0500

    "longview" logo svg and animations are good

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -2,24 +2,22 @@
 import './css/Logo.css';
 
 export function Logo() {
+	const letters = ['o', 'n', 'g', 'v', 'i', 'e', 'w'];
 	return (
-		<svg width="256px" height="128px" viewBox="0 0 128 256" id="logo" xmlns="http://www.w3.org/2000/svg">
+		<svg width="500px" height="128px" viewBox="0 0 128 256" id="logo" xmlns="http://www.w3.org/2000/svg">
 			<path
 				fill="none"
 				strokeLinecap="round"
 				strokeWidth="5"
 				stroke="#000"
 				id="logo-l"
-				d="M 6, 250 L 106, 250 L 106, 6"
+				d="M 66, 250 L 166, 250 L 166, 6"
 			/>
-			<path
-				fill="none"
-				strokeLinecap="round"
-				strokeWidth="5"
-				stroke="#000"
-				id="logo-o"
-				d="M 12,5 C 2,2 8,2 8,5 M2,5 C2,8 8,8 8,5"
-			/>
+			{letters.map((l, i) => (
+				<text id={"logo"+i} key={i} x={((i / 300) * 10000 - 115).toString()} fontSize="32" y="200">
+					{l}
+				</text>
+			))}
 		</svg>
 	);
 }
