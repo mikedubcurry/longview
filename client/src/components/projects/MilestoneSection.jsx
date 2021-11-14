5e1f6fd41af49a3f65569333a2704a7ef067519d
Michael Curry <michaelcurry95@gmail.com>
2021-11-14 10:21:13 -0500

    some hover styles and render full milestonesection

--- client/src/components/projects/MilestoneSection.jsx
+++ client/src/components/projects/MilestoneSection.jsx
@@ -3,25 +3,21 @@ import { useState } from 'react';
 export function MilestoneSection({ milestones }) {
 	const [expanded, setExpanded] = useState(false);
 
-	if (expanded) {
-		return (
-			<div className="project-milestone" onClick={() => setExpanded(false)}>
-				{milestones.map((ms) => (
-          <p key={ms.id} className={'milestone' + (ms.completed ? ' milestone-completed' : '')}>{ms.milestone}</p>
-          ))}
-          <progress
-            className="project-milestone-progress"
-            max={milestones.length}
-            value={milestones.filter((i) => i.completed).length}
-          />
-			</div>
-		);
-	}
 	return (
-		<div className="project-milestone" onClick={() => setExpanded(true)}>
-			<span className="project-milestone-progress-count">
-				{milestones.filter((m) => m.completed).length} / {milestones.length} milestones complete
-			</span>
+		<div className={'project-milestone' + (expanded ? ' expanded' : '')} onClick={() => setExpanded(!expanded)}>
+			{expanded ? (
+				<>
+					{milestones.map((ms) => (
+						<p key={ms.id} className={'milestone' + (ms.completed ? ' milestone-completed' : '')}>
+							{ms.milestone}
+						</p>
+					))}
+				</>
+			) : (
+				<span className="project-milestone-progress-count">
+					{milestones.filter((m) => m.completed).length} / {milestones.length} milestones complete
+				</span>
+			)}
 			<progress
 				className="project-milestone-progress"
 				max={milestones.length}
