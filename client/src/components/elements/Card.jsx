d82d0b6e3f3a695e852c87163ca9778185c2b69b
Michael Curry <michaelcurry95@gmail.com>
2021-11-21 16:19:40 -0500

    all emotion no css files...

--- client/src/components/elements/Card.jsx
+++ client/src/components/elements/Card.jsx
@@ -1,8 +1,64 @@
-import './css/Card.css';
+// import './css/Card.css';
 
 export function Card({ header, text, children }) {
 	return (
-		<div className="card">
+		<div
+			css={css`
+				& {
+					margin-bottom: 1rem;
+					min-width: 500px;
+					max-width: fit-content;
+					padding: 1.25rem 1rem;
+					border-radius: 6px;
+
+					display: flex;
+					flex-direction: column;
+					align-items: flex-start;
+
+					background-color: var(--base-card-bg);
+
+					transition: background-color 0.25s ease;
+				}
+
+				.card:hover {
+					background-color: var(--base-card-hover);
+				}
+
+				& header {
+					color: black;
+					font-size: 24px;
+					text-decoration: underline;
+					text-decoration-thickness: 1px;
+					align-self: flex-start;
+				}
+
+				& header a {
+					color: unset;
+					text-decoration: none;
+					transition: color 0.3s ease;
+				}
+
+				& header a:hover {
+					color: blue;
+				}
+
+				& .card-text {
+					align-self: flex-start;
+				}
+
+				.card-content {
+					width: 100%;
+					padding: 1rem;
+					margin-top: 0.5rem;
+					border-radius: 4px;
+
+					/* background-color: var(--base-card-subsection-bg); */
+
+					display: flex;
+					flex-direction: column;
+				}
+			`}
+		>
 			<header className="card-header">{header}</header>
 			<div className="card-text">{text}</div>
 			<div className="card-content">{children}</div>
--- client/src/components/elements/Modal.jsx
+++ client/src/components/elements/Modal.jsx
@@ -2,7 +2,6 @@
 import { useState, useEffect, useRef } from 'react';
 
 import { Portal } from './Portal';
-import './css/Modal.css'
 
 export function Modal({ children, open, onClose, locked }) {
 	// set up active state
@@ -54,7 +53,57 @@ export function Modal({ children, open, onClose, locked }) {
 	return (
 		<>
 			{(open || active) && (
-				<Portal className="modal-portal">
+				<Portal
+					css={css`
+						.contents {
+							position: relative;
+							padding: 20px;
+							box-sizing: border-box;
+							min-height: 50px;
+							min-width: 50px;
+							max-height: 80%;
+							max-width: 80%;
+							box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+							background-color: white;
+							border-radius: 2px;
+						}
+
+						.backdrop {
+							position: fixed;
+							top: 0;
+							right: 0;
+							bottom: 0;
+							left: 0;
+							background-color: rgba(51, 51, 51, 0.3);
+							backdrop-filter: blur(1px);
+							opacity: 0;
+							transition: all 100ms cubic-bezier(0.4, 0, 0.2, 1);
+							transition-delay: 200ms;
+							display: flex;
+							align-items: center;
+							justify-content: center;
+						}
+
+						.backdrop .modal-content {
+							transform: translateY(100px);
+							transition: all 200ms cubic-bezier(0.4, 0, 0.2, 1);
+							opacity: 0;
+						}
+
+						.backdrop.active {
+							transition-duration: 250ms;
+							transition-delay: 0ms;
+							opacity: 1;
+						}
+						.backdrop.active .modal-content {
+							transform: translateY(0);
+							opacity: 1;
+							transition-delay: 150ms;
+							transition-duration: 350ms;
+						}
+					`}
+					className="modal-portal"
+				>
 					<div ref={backdrop} className={`backdrop ${active && open && 'active'}`}>
 						<div className="modal-content contents">{children}</div>
 					</div>
