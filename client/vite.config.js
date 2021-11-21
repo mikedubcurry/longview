055a5d3a55570f2f805560b8622abcaa9919d53b
Michael Curry <michaelcurry95@gmail.com>
2021-11-21 15:55:08 -0500

    add emotion to vite build process

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3001
-  }
-})
+	plugins: [
+		react({
+			jsxImportSource: '@emotion/react',
+			babel: {
+				plugins: ['@emotion/babel-plugin'],
+			},
+		}),
+	],
+	server: {
+		port: 3001,
+	},
+	esbuild: {
+		jsxFactory: `jsx`,
+		jsxInject: `import {jsx, css} from '@emotion/react'`,
+	},
+});
