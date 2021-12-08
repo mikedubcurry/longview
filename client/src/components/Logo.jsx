230c9161efb91dfb117328807d3cd647aa6beb4a
Michael Curry <michaelcurry95@gmail.com>
2021-12-08 14:12:48 -0500

    smaller and compact svg path, centered in a gross way

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,53 +1,56 @@
 const styles = css`
+	position: relative;
+	top: -7px;
+	left: -5px;
 	#logo-l {
-		transform-origin: 150% 100%;
+		transform-origin: 90% 82%;
 		animation: fallover 1s ease forwards;
 	}
 
 	#logo0 {
-		transform: translateY(100%);
+		transform: translateY(150%);
 		transition: transform;
 		animation: popup 0.5s cubic-bezier(0.3, 0.7, 0.4, 1.5) 1s forwards;
 	}
 	#logo1 {
-		transform: translateY(100%);
+		transform: translateY(150%);
 		transition: transform;
 		animation: popup 0.5s cubic-bezier(0.3, 0.7, 0.4, 1.5) 1.3s forwards;
 	}
 	#logo2 {
-		transform: translateY(100%);
+		transform: translateY(150%);
 		transition: transform;
 		animation: popup 0.5s cubic-bezier(0.3, 0.7, 0.4, 1.5) 1.6s forwards;
 	}
 	#logo3 {
-		transform: translateY(100%);
+		transform: translateY(150%);
 		transition: transform;
 		animation: popup 0.5s cubic-bezier(0.3, 0.7, 0.4, 1.5) 1.9s forwards;
 	}
 	#logo4 {
-		transform: translateY(100%);
+		transform: translateY(150%);
 		transition: transform;
 		animation: popupI 0.5s cubic-bezier(0.3, 0.7, 0.4, 1.5) 2.2s forwards;
 	}
 	#logo5 {
-		transform: translateY(100%);
+		transform: translateY(150%);
 		transition: transform;
 		animation: popup 0.5s cubic-bezier(0.3, 0.7, 0.4, 1.5) 2.5s forwards;
 	}
 	#logo6 {
-		transform: translateY(100%);
+		transform: translateY(150%);
 		transition: transform;
 		animation: popup 0.5s cubic-bezier(0.3, 0.7, 0.4, 1.5) 2.8s forwards;
 	}
 
 	@keyframes popup {
 		to {
-			transform: translateY(0);
+			transform: translateY(-15%);
 		}
 	}
 	@keyframes popupI {
 		to {
-			transform: translateY(0) translateX(6px);
+			transform: translateY(-15%) translateX(6px);
 		}
 	}
 
@@ -59,7 +62,7 @@ transform: rotate(90deg) translateY(100%);
 			transform: rotate(90deg);
 		}
 		100% {
-			transform: rotate(90deg) translateY(130%);
+			transform: rotate(90deg) translateY(100%);
 		}
 	}
 `;
@@ -68,14 +71,22 @@ transform: rotate(90deg) translateY(100%);
 export function Logo() {
 	const letters = ['o', 'n', 'g', 'v', 'i', 'e', 'w'];
 	return (
-		<svg css={styles} width="500px" height="128px" viewBox="0 0 128 256" id="logo" xmlns="http://www.w3.org/2000/svg">
+		<svg
+			aria-label="Longview logo"
+			css={styles}
+			width="200px"
+			height="100px"
+			viewBox="0 0 128 256"
+			id="logo"
+			xmlns="http://www.w3.org/2000/svg"
+		>
 			<path
 				fill="none"
 				strokeLinecap="round"
 				strokeWidth="5"
 				stroke="#000"
 				id="logo-l"
-				d="M 66, 250 L 166, 250 L 166, 6"
+				d="M 0, 210 l 100, 0 l 0, -250"
 			/>
 			{letters.map((l, i) => (
 				<text id={'logo' + i} key={i} x={((i / 300) * 10000 - 115).toString()} fontSize="32" y="200">
