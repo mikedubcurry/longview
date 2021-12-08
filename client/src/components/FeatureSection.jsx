61d75378298c912ea10b3eb7edd8b940e9e3b4ed
Michael Curry <michaelcurry95@gmail.com>
2021-12-08 13:44:53 -0500

    color gradients are better

--- client/src/components/FeatureSection.jsx
+++ client/src/components/FeatureSection.jsx
@@ -5,15 +5,23 @@ const styles = (leftAlign) => css`
 	height: 80vh;
 	display: grid;
 	grid-template-rows: calc(13vh + var(--med-padding) * 2) auto;
-	background-color: ${leftAlign ? 'blue' : 'orange'};
+	background-color: ${leftAlign ? 'darkblue' : 'orange'};
+	background-image: ${leftAlign
+		? 'linear-gradient( to top left, blue , darkblue 50%)'
+		: 'linear-gradient(to top right	,lightcoral , orange 25%)'};
+
 	.feature-section-icon {
 		background-color: ${leftAlign ? 'orange' : 'rgb(33, 128, 119)'};
+
 
 		.icon {
 			float: ${leftAlign ? 'left' : 'right'};
 			width: 58%;
 			height: 100%;
-			background-color: ${leftAlign ? 'blue' : 'orange'};
+			background-color: ${leftAlign ? 'darkblue' : 'orange'};
+			background-image: ${leftAlign
+				? 'linear-gradient( to bottom right, blue , darkblue 50%)'
+				: 'linear-gradient(to bottom left	,lightcoral , orange 50%)'};
 			border-radius: ${leftAlign ? '0 var(--med-padding) 0 0' : 'var(--med-padding) 0 0 0'};
 			display: flex;
 			justify-content: center;
@@ -40,7 +48,7 @@ const styles = (leftAlign) => css`
 		.convex-corner::before {
 			position: absolute;
 			content: '';
-			background-color: ${leftAlign ? 'blue' : 'orange'};
+			background-color: ${leftAlign ? 'darkblue' : 'orange'};
 			// background-color: red;
 			bottom: 0;
 			width: calc(var(--med-padding));
@@ -50,6 +58,7 @@ const styles = (leftAlign) => css`
 	}
 
 	.feature-section-text {
+		color: ${leftAlign ? '#eee' : '#222'};
 		padding: var(--med-padding);
 	}
 `;
